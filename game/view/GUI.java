324e9514a6f9764f34ade9880d869a9cb97e4954
Dipen Patel <dipen70@gmail.com>
2016-09-07 13:45:54 +1200

    first commit

--- game/view/GUI.java
+++ game/view/GUI.java
@@ -1,9 +1,9 @@
 package game.view;
 /**
- * This class represents the main GUI class this class contains all the 
+ * This class represents the main GUI class this class bring together all the different components of the GUI. 
  * @author Dipen
  *
  */
 public class GUI {
-//dsdss
+	
 }
