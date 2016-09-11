6b4a84190cd739459323344a25a716f3f77ac060
Dipen <dipen70@gmail.com>
2016-09-12 07:25:30 +1200

    gui almost complete

--- game/view/GUI.java
+++ game/view/GUI.java
@@ -3,19 +3,20 @@ package game.view;
 import javafx.*;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 
 import javafx.application.*;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
+import javafx.scene.control.*;
+import javafx.scene.layout.StackPane;
+import javafx.scene.image.ImageView;
+import javafx.geometry.Insets;
 
 /**
  * This class represents the main GUI class this class bring together all the
@@ -28,7 +29,18 @@ public class GUI extends Application {
 	private static final String GAMEICON_IMAGE = "/game-icon.png";
 	private static int WIDTH_VALUE = 1500;
 	private static int HEIGHT_VALUE = 1000;
+	// main window
 	private Stage window;
+	// controls
+	private MenuBar menuBar;
+	private Label timeLable;
+	private Label miniMapLable;
+	private Label textAreaLable;
+	// panes
+	// right pane with vertical alligment
+	private VBox vbox;
+	// standard layout
+	private BorderPane borderPane;
 
 	public GUI() {
 	}
@@ -41,7 +53,30 @@ public class GUI extends Application {
 	public void start(Stage mainWindow) throws Exception {
 		this.window = mainWindow;
 		window.setTitle("Plague Game");
-		//window.getIcons().add(loadImage(GAMEICON_IMAGE));
+		window.getIcons().add(loadImage(GAMEICON_IMAGE));
+		window.setResizable(false);
+
+		// Create a VBox which is just layout manger and adds gap of 10
+		vbox = new VBox(10);
+		vbox.setPrefSize(400, 1000);
+		// vbox.setStyle("-fx-background-color: #242731;");
+		// vbox.setPadding(new Insets(10, 50, 50, 50));
+		vbox.getStyleClass().add("cotrolvbox");
+		borderPane = new BorderPane();
+		borderPane.setRight(vbox);
+		// creates a scene
+		setMenuBar();
+		setWorldTime();
+		setminiMap();
+		setchat();
+		Scene scene = new Scene(borderPane, WIDTH_VALUE, HEIGHT_VALUE);
+		scene.getStylesheets().add(this.getClass().getResource("/main.css").toExternalForm());
+		window.setScene(scene);
+		window.show();
+
+	}
+
+	private void setMenuBar() {
 		// creates the menuBar
 		MenuBar menuBar = new MenuBar();
 		// creates the menu
@@ -61,18 +96,43 @@ public class GUI extends Application {
 		help.getItems().addAll(itmInfo, itmAbout);
 		// adds menus to menu Bar
 		menuBar.getMenus().addAll(file, help);
-		// Create a VBox which is just layout manger
-		VBox vbox = new VBox();
-		vbox.setPrefSize(400, 1000);
-		vbox.setStyle("-fx-background-color: #336699;");
-		BorderPane borderPane = new BorderPane();
 		// add the layout to the borderPane Layout
 		borderPane.setTop(menuBar);
-		borderPane.setRight(vbox);
-		// creates a scene
-		Scene scene = new Scene(borderPane, WIDTH_VALUE, HEIGHT_VALUE);
-		window.setScene(scene);
-		window.show();
+	}
+
+	private void setWorldTime() {
+		TitledPane titlePane = new TitledPane();
+		titlePane.setText("World Time");
+		timeLable = new Label();
+		titlePane.setContent(timeLable);
+		timeLable.setPrefWidth(400);
+		timeLable.setPrefHeight(50);
+		timeLable.getStyleClass().add("world-time-lable");
+		timeLable.setText("00:00");
+		vbox.getChildren().add(titlePane);
+	}
+
+	public void setminiMap() {
+		TitledPane titlePane = new TitledPane();
+		titlePane.setText("Mini Map");
+		miniMapLable = new Label();
+		titlePane.setContent(miniMapLable);
+		miniMapLable.setPrefWidth(400);
+		miniMapLable.setPrefHeight(370);
+		miniMapLable.getStyleClass().add("minimap-lable");
+		vbox.getChildren().add(titlePane);
+	}
+
+	public void setchat() {
+		TitledPane titlePane = new TitledPane();
+		VBox chatControls = new VBox(2);
+		titlePane.setText("Chat Room");
+		chatControls.setPrefWidth(400);
+		chatControls.setPrefHeight(200);
+		chatControls.getStyleClass().add("chatarea-background");
+		titlePane.setContent(chatControls);
+		
+		vbox.getChildren().add(titlePane);
 
 	}
 
