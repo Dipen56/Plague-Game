b86db1c19c8b2ba6eef0ea6333647c5685f2b40d
dr0wranger <tintabay@yahoo.com>
2016-09-15 10:58:42 +1200

    Network is now working

--- client/ThreadClient.java
+++ client/ThreadClient.java
@@ -4,15 +4,15 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.PortUnreachableException;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
 import client.view.GUI;
-import server.Packet;
+import server.Packet.DataType;
 import server.PacketTypes;
 import server.Server;
-import server.Packet.DataType;
-import server.PacketTypes.Message;;
+import server.Packet;;
 
 
 //import game.control.Packet.DataType;
@@ -23,11 +23,11 @@ public class ThreadClient extends Thread {
 	private InetAddress ipAddress;
 	private GUI gui;
 
-	public ThreadClient(GUI g) {
+	public ThreadClient(String add, GUI g) {
 		this.gui = g;
 		try {
 			clientSocket = new DatagramSocket();
-			ipAddress = InetAddress.getByName("localhost");
+			ipAddress = InetAddress.getByName(add);
 			clientSocket.connect(ipAddress, Server.PORTN_NUM);
 		} catch (SocketException e) {
 			e.getMessage();
@@ -41,44 +41,52 @@ public class ThreadClient extends Thread {
 		while (true) {
 			try {
 				sleep(10); //sleep for 10 ms and wait for reply from server
+
 			} catch (InterruptedException e) {
 				e.getMessage();
 			}
-			
+
 			byte[] message = new byte[5000];
 			DatagramPacket packet = new DatagramPacket(message, message.length);
-			
+
 			try {
 				clientSocket.receive(packet);
+			} catch (PortUnreachableException e) {
+				e.getCause();
 			} catch (IOException e) {
-				e.getMessage();
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+			//clientSocket.close();
 			readMessage(packet);
 		}
 	}
-	
+
 	/**
-	 * Extracts the type of packet the client had received and calls the appropriate method 
+	 * Extracts the type of packet the client had received and calls the appropriate method
 	 * to handle the data
 	 * @param dPacket the packet to be read and extracted
 	 */
 	 public void readMessage(DatagramPacket dPacket) {
-		
+
+		 System.out.println("recieved");
+
+
 		DataType type = Packet.getPacketType(new String(dPacket.getData()).trim().substring(0, 1));
-		
+
 		if(type.equals(DataType.MESSAGE)){
 			PacketTypes p = new PacketTypes();
 			PacketTypes.Message m = p.new Message(dPacket.getData());
 			System.out.println(m.toString());
 		}
-		
+
 
 	}
 	/**
 	 * 	Makes a new Datagram object and sends it to the server
 	 * @param data bytes of information to be sent
 	 */
-	synchronized public void sendDataToServer(byte[] message) {
+	 public void sendDataToServer(byte[] message) {
 		  DatagramPacket packet = new DatagramPacket(message, message.length, ipAddress, Server.PORTN_NUM);
 	        try {
 	            clientSocket.send(packet);
@@ -87,17 +95,22 @@ public class ThreadClient extends Thread {
 	        }
 	}
 
-	
-	
-	public String getClientAddress(){
-		return this.ipAddress.getHostAddress();
+
+
+	public InetAddress getClientAddress(){
+	try {
+		return InetAddress.getLocalHost();
+	} catch (UnknownHostException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
 	}
-	
+	return null;
+	}
+
+
 	public int getClientPort(){
 		return this.clientSocket.getPort();
 	}
-	
-	public void closeSocket(){
-		this.clientSocket.close();
-	}
+
+
 }
