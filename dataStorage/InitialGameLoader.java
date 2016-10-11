f02a70e89633d0ea04efce95f57505af6bb6e1a0
danaxe7 <daniel.dd.anastasi@outlook.com>
2016-10-11 14:11:11 +1300

    Locked doors

--- dataStorage/InitialGameLoader.java
+++ dataStorage/InitialGameLoader.java
@@ -306,13 +306,13 @@ public class InitialGameLoader {
 
 		// Puts the rooms together.
 		// TODO: change back to true
-		Room room1 = new Room(room1Map, areaIDs.get(1), keys.get(keys.size() - 4).getKeyID(), false,
+		Room room1 = new Room(room1Map, areaIDs.get(1), keys.get(keys.size() - 4).getKeyID(), true,
 				"Looks like a tidy room");
-		Room room2 = new Room(room2Map, areaIDs.get(2), keys.get(keys.size() - 3).getKeyID(), false,
+		Room room2 = new Room(room2Map, areaIDs.get(2), keys.get(keys.size() - 3).getKeyID(), true,
 				"Has someone been here before?");
-		Room room3 = new Room(room3Map, areaIDs.get(3), keys.get(keys.size() - 2).getKeyID(), false,
+		Room room3 = new Room(room3Map, areaIDs.get(3), keys.get(keys.size() - 2).getKeyID(), true,
 				"This is actually a sunny warm room");
-		Room room4 = new Room(room4Map, areaIDs.get(4), keys.get(keys.size() - 1).getKeyID(), false,
+		Room room4 = new Room(room4Map, areaIDs.get(4), keys.get(keys.size() - 1).getKeyID(), true,
 				"Something stinks in this room");
 
 		MapElement[][] worldMap = {
